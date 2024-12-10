7334c300d62f1579b060b67f40b71493c0071ed4
samarsrivastav <samarsrivastav69@gmail.com>
2024-12-10 13:20:07 +0530

    PRE-KDU-JAVA: Solved

--- java/src/main/java/com/prekdu/CSVWordFrequency.java
+++ java/src/main/java/com/prekdu/CSVWordFrequency.java
@@ -1,10 +1,45 @@
 package com.prekdu;
 
-/*
- * Create a basic Java Program that reads a CSV file and prints the top 3 repeated words in the file.
- * The CSV file is in the reosurces folder and the file name is input.csv.
- */
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
-public class CSVWordFrequency {
-  public static void main(String[] args) {}
+public final class CSVWordFrequency {
+  /** the length required in question. */
+  private static final int LENGTH = 3;
+
+  private CSVWordFrequency() {
+    throw new UnsupportedOperationException("Utility class");
+  }
+
+  /**
+   * The main method where the program execution begins.
+   *
+   * @param args command-line arguments (not used in this program)
+   */
+  public static void main(final String[] args) {
+    try {
+      String file = "java/src/main/resources/input.csv";
+      HashMap<String, Integer> mp = new HashMap<>();
+      Scanner sc = new Scanner(new File(file));
+      while (sc.hasNextLine()) {
+        String[] temp = sc.nextLine().split(",");
+        for (String x : temp) {
+          mp.put(x, mp.getOrDefault(x, 0) + 1);
+        }
+      }
+      List<Map.Entry<String, Integer>> arr = new ArrayList<>(mp.entrySet());
+      arr.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+
+      for (int i = 0; i < Math.min(LENGTH, arr.size()); i++) {
+        System.out.println(arr.get(i));
+      }
+      sc.close();
+    } catch (Exception e) {
+      System.err.println("error fetching file");
+    }
+  }
 }
